cac28d29215a94c48adf5d1fde9daa071d8f83c5
serraguti <pacoserranox@gmail.com>
2023-01-26 21:00:58 +0100

    Add project files.

---
+++ CustomersApiNuget2023/CustomerContext.cs
@@ -0,0 +1,38 @@
+﻿using CustomersApiNuget2023.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersApiNuget2023
+{
+    public class CustomerContext
+    {
+        //VAMOS A LEER TODOS LOS CUSTOMER DE UN API DE INTERNET
+        //NECESITAMOS QUE EL METODO SEA ASINCRONO
+        public async Task<CustomerList> GetCustomerListAsync()
+        {
+            WebClient client = new WebClient();
+            //INDICAMOS QUE TIPO DE DATO VAMOS A LEER
+            client.Headers["content-type"] = "application/json";
+            string urlApi = "https://services.odata.org/V4/Northwind/Northwind.svc/Customers";
+            //LEEMOS EL API Y DESCARGAMOS TODO EL CONTENIDO (string) JSON
+            string json = await client.DownloadStringTaskAsync(urlApi);
+            //COMO LOS DATOS VIENEN DIRECTOS COMO COLECCION/ARRAY
+            CustomerList customers =
+               JsonConvert.DeserializeObject<CustomerList>(json);
+
+
+
+
+            //LA LIBRERIA NewtonSoft.json CON LOS MAPEOS QUE HEMOS REALIZADO
+            //AUTOMATICAMENTE ENLAZA CADA PROPIEDAD CON CADA DATO DEL JSON
+            CustomerList customers =
+                JsonConvert.DeserializeObject<CustomerList>(json);
+            return customers;
+        }
+    }
+}
---
+++ CustomersApiNuget2023/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomersApiNuget2023.Models
+{
+    public class Customer
+    {
+        [JsonProperty("CustomerID")]
+        public string IdCustomer { get; set; }
+        [JsonProperty("CompanyName")]
+        public string Company { get; set; }
+        [JsonProperty("ContactName")]
+        public string Name { get; set; }
+        [JsonProperty("City")]
+        public string City { get; set; }
+    }
+}
